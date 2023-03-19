40f7abca497a75c382a5d080d6fa87af23425a82
Juno <ganimedes409@gmail.com>
2023-03-19 11:45:55 -0300

    feat: add docker config api

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -7,34 +7,35 @@ export class InMemoryRepository implements UsersRepository {
 
     async findById(id: string): Promise<User | null> {
         const user = this.items.find(item => item.id === id)
-        
+
         if (!user) {
             return null
         }
         return user
-    }   
+    }
 
-    
+
     async findByEmail(email: string): Promise<User | null> {
         const user = this.items.find(item => item.email === email)
-        
+
         if (!user) {
             return null
         }
         return user
-    }   
+    }
 
-   async create(data: Prisma.UserCreateInput) {
+    async create(data: Prisma.UserCreateInput): Promise<User> {
         const user = {
             id: randomUUID(),
             name: data.name,
             email: data.email,
+            role: data.role,
             password_hash: data.password_hash,
             created_at: new Date(),
         }
-        
-        this.items.push(user)  
-        return user    
+
+        this.items.push(user)
+        return user
     }
 
 } 
